fff5214204c92f5ff383c8bfa87c738db75757f7
amiceli <antoine@miceli.click>
2020-10-21 10:12:50 +0200

    Re-run process after fail.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
 const axios = require('axios')
+const readline = require('readline')
 const signale = require('./src/Signale.js')
 const PipelinesApi = require('./src/PipelinesApi')
 const Display = require('./src/Display')
 
-const readline = require('readline')
 readline.emitKeypressEvents(process.stdin)
 process.stdin.setRawMode(true)
 
-const { project, limit, token, url } = require('./src/init')(process.argv.slice(2))
+const {
+    project, limit, token, url,
+} = require('./src/init')(process.argv.slice(2))
 
 process.stdin.on('keypress', (str, key) => {
     console.log(key.name)
@@ -25,15 +27,15 @@ const run = async () => {
 
         process.stdout.write('\x1Bc')
 
-		const webUrl = pipelines[0].web_url.split('pipeline')[0]
+        const webUrl = pipelines[0].web_url.split('pipeline')[0]
 
         console.log(`Repository ${repository.name}`)
         console.log(`Url        ${webUrl}`)
         console.log('')
 
-        for (let pipeline of pipelines) {
+        pipelines.forEach((pipeline) => {
             Display.showPipelines(pipeline)
-        }
+        })
 
         console.log('')
         console.log('Press q key to exit...')
@@ -41,6 +43,7 @@ const run = async () => {
         setTimeout(run, 5000)
     } catch (e) {
         signale.fatal(e)
+        setTimeout(run, 2000)
     }
 }
 
